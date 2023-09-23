0f69008e59002d7197fa786b78b4cbe0d04a081c
Hamza Waheed <137566252+hmzi67@users.noreply.github.com>
2023-09-23 03:19:17 -0700

    update

--- class10 Task/index.js
+++ class10 Task/index.js
@@ -1,13 +1,75 @@
 "use strict";
+// Name     :   Hamza Waheed Abbasi
+//Roll no   :   PIAIC215406
+//Batch     :   48
+//City      :   Islamabad
+//Email     :   hamzawaheed057@gmail.com
+Object.defineProperty(exports, "__esModule", { value: true });
+// Difference b/w callback and promises.
+// ____________________________________________________________________________________________________
+/*
+Call backs:
+    Syntax:
+        Callbacks are functions that are passed as arguments to other functions and are executed once the asynchronous operation is complete.
+
+    Readability:
+        Callbacks can lead to callback hell or pyramid of doom when dealing with multiple asynchronous operations, making the code less readable and maintainable.
+        
+    Error Handling:
+        Error handling in callback-based code can be challenging as errors need to be passed as separate callback arguments.
+*/
+console.log("");
+console.log("Following will be the output of callback functions");
+function born(cb) {
+    cb("I'm born now...");
+}
+function childhood(cb) {
+    cb("Now I'm 3 year old");
+}
+function jwani(cb) {
+    cb("Now I'm Jawaan");
+}
+function bhurhapa(cb) {
+    cb("Now I'm Bhudda");
+}
+function death(cb) {
+    cb("Now I'm dead");
+}
+born((t) => {
+    console.log(t);
+    childhood((t) => {
+        console.log(t);
+        jwani((t) => {
+            console.log(t);
+            bhurhapa((t) => {
+                console.log(t);
+                death((t) => {
+                    console.log(t);
+                });
+            });
+        });
+    });
+});
+/*
+Promises:
+    Syntax:
+        Promises provide a more structured way to handle asynchronous operations. They represent a value that may be available now, or in the future, or never. Promises have methods like then and catch to handle success and error cases.
+
+    Readability:
+        Promises offer a more linear and readable code structure, especially when chaining multiple asynchronous operations using .then().
+
+    Error Handling:
+        Error handling is cleaner with Promises using the .catch() method, which makes it easier to manage errors in asynchronous code.
+*/
+console.log("");
+console.log("Following will be the output of Promises");
 function carMaintenance(num) {
     console.log("Leave car at workshop");
     console.log("Do Grocery....");
     let result = new Promise((resolve, reject) => {
         if (num > 3) {
-            setTimeout(() => {
-                console.log("Car is fixed");
-                resolve("Your car is ready...");
-            }, 3000);
+            console.log("Car is fixed");
+            resolve("Your car is ready...");
         }
         else {
             reject("Car is not ready");
@@ -18,10 +80,8 @@ function carMaintenance(num) {
 function pickDress(num) {
     let result = new Promise((resolve, reject) => {
         if (num > 3) {
-            setTimeout(() => {
-                console.log("dress is ironed");
-                resolve("Your dress is ready...");
-            }, 3000);
+            console.log("dress is ironed");
+            resolve("Your dress is ready...");
         }
         else {
             reject("dress is not ready");
@@ -32,9 +92,7 @@ function pickDress(num) {
 function attendEvent(num) {
     let result = new Promise((resolve, reject) => {
         if (num > 3) {
-            setTimeout(() => {
-                resolve("Attend event...");
-            }, 3000);
+            resolve("Attend event...");
         }
         else {
             reject("event is not ended");
--- class10 Task/index.ts
+++ class10 Task/index.ts
@@ -4,29 +4,89 @@
 //City      :   Islamabad
 //Email     :   hamzawaheed057@gmail.com
 
+
+// Difference b/w callback and promises.
+
+// ____________________________________________________________________________________________________
+/* 
+Call backs:
+    Syntax: 
+        Callbacks are functions that are passed as arguments to other functions and are executed once the asynchronous operation is complete.
+
+    Readability: 
+        Callbacks can lead to callback hell or pyramid of doom when dealing with multiple asynchronous operations, making the code less readable and maintainable.
+        
+    Error Handling: 
+        Error handling in callback-based code can be challenging as errors need to be passed as separate callback arguments.
+*/
+console.log("");
+console.log("Following will be the output of callback functions");
+function born(cb: (txt: string) => void) {
+  cb("I'm born now...");
+}
+function childhood(cb: (txt: string) => void) {
+  cb("Now I'm 3 year old");
+}
+function jwani(cb: (txt: string) => void) {
+  cb("Now I'm Jawaan");
+}
+function bhurhapa(cb: (txt: string) => void) {
+  cb("Now I'm Bhudda");
+}
+function death(cb: (txt: string) => void) {
+  cb("Now I'm dead");
+}
+
+born((t: string) => {
+  console.log(t);
+  childhood((t: string) => {
+    console.log(t);
+    jwani((t: string) => {
+      console.log(t);
+      bhurhapa((t: string) => {
+        console.log(t);
+        death((t: string) => {
+          console.log(t);
+        });
+      });
+    });
+  });
+});
+
+/*
+Promises:
+    Syntax: 
+        Promises provide a more structured way to handle asynchronous operations. They represent a value that may be available now, or in the future, or never. Promises have methods like then and catch to handle success and error cases.
+
+    Readability: 
+        Promises offer a more linear and readable code structure, especially when chaining multiple asynchronous operations using .then().
+
+    Error Handling: 
+        Error handling is cleaner with Promises using the .catch() method, which makes it easier to manage errors in asynchronous code. 
+*/
+
+
+console.log("");
+console.log("Following will be the output of Promises");
+
 function carMaintenance(num: number) {
   console.log("Leave car at workshop");
   console.log("Do Grocery....");
   let result: Promise<string> = new Promise((resolve, reject) => {
     if (num > 3) {
-      setTimeout(() => {
-        console.log("Car is fixed");
-        resolve("Your car is ready...");
-      }, 3000);
+      console.log("Car is fixed");
+      resolve("Your car is ready...");
     } else {
       reject("Car is not ready");
     }
   });
   return result;
 }
-
 function pickDress(num: number) {
   let result: Promise<string> = new Promise((resolve, reject) => {
     if (num > 3) {
-      setTimeout(() => {
-        console.log("dress is ironed");
-        resolve("Your dress is ready...");
-      }, 3000);
+      console.log("dress is ironed");
+      resolve("Your dress is ready...");
     } else {
       reject("dress is not ready");
     }
@@ -37,9 +97,7 @@ function pickDress(num: number) {
 function attendEvent(num: number) {
   let result: Promise<string> = new Promise((resolve, reject) => {
     if (num > 3) {
-      setTimeout(() => {
-        resolve("Attend event...");
-      }, 3000);
+      resolve("Attend event...");
     } else {
       reject("event is not ended");
     }
@@ -63,31 +121,4 @@ machanicResponce
   .catch((err: any) => {
     console.log("error", err);
   });
-
-
-// Difference b/w callback and promises.
-
-/* 
-Call backs:
-    Syntax: 
-        Callbacks are functions that are passed as arguments to other functions and are executed once the asynchronous operation is complete.
-
-    Readability: 
-        Callbacks can lead to callback hell or pyramid of doom when dealing with multiple asynchronous operations, making the code less readable and maintainable.
-        
-    Error Handling: 
-        Error handling in callback-based code can be challenging as errors need to be passed as separate callback arguments.
-*/
-
-/*
-Promises:
-    Syntax: 
-        Promises provide a more structured way to handle asynchronous operations. They represent a value that may be available now, or in the future, or never. Promises have methods like then and catch to handle success and error cases.
-
-    Readability: 
-        Promises offer a more linear and readable code structure, especially when chaining multiple asynchronous operations using .then().
-
-    Error Handling: 
-        Error handling is cleaner with Promises using the .catch() method, which makes it easier to manage errors in asynchronous code. 
-*/
 
